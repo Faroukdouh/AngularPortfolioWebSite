1c8fbfc6fcfe82208f56379b0d25fcc93aee7089
Faroukdouh <faroukdouh06@gmail.com>
2024-09-28 20:50:05 +0200

    portfolioPerson v1

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,14 +1,23 @@
 export class Tag{
 
+    
+    static readonly JAVA = new Tag('Java','orange', false);
+    static readonly JavaFx = new Tag('JavaFx','red', false);
+    static readonly JavaEE = new Tag('JavaEE','red', false);
+    static readonly SpringBoot = new Tag('SpringBoot','red', false);
+    static readonly HTMLCSS = new Tag('HTML/CSS','red', false);
+    static readonly JSF = new Tag('JSF','red', false);
+    static readonly PrimeFaces = new Tag('PrimeFaces','red', false);
+    static readonly Bootstrap = new Tag('Bootstrap','red', false);
     static readonly ANGULAR = new Tag('Angular','red', false);
     static readonly TYPESCIPT = new Tag('TypeScript','darkred', false);
-    static readonly JAVA = new Tag('Java','orange', false);
     static readonly JAVASCRIPT = new Tag('JavaScript','green', false);
-    static readonly PYTHON = new Tag('PYTHON','red', false);
-    static readonly NODEJS = new Tag('NODEJS','red', false);
-    static readonly CSHARP = new Tag('CSHARP','red', false);
-    static readonly ASPNET = new Tag('ASPNET','red', false);
-    static readonly REACT = new Tag('REACT','red', false);
+    static readonly PHP = new Tag('PHP','red', false);
+    static readonly UML = new Tag('UML','red', false);
+    static readonly Git = new Tag('Git','red', false);
+    static readonly ScrumMethod = new Tag('Scrum Method','red', false);
+    
+    
 
     private constructor( private readonly key: string,  public readonly color: string, public filtering : boolean){
 
--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -8,38 +8,39 @@ import { Tag } from '../_models/Tag';
 export class ProjectsService {
 
   projects: Project[] = [
-    {id: 0, name: "Sample Python Project", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Python project that analyzes stock market data.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.PYTHON]},
-    {id: 1, name: "Sample Angular App", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web app developed using Angular and Node.JS", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.ANGULAR, Tag.TYPESCIPT, Tag.NODEJS]},
-    {id: 2, name: "Sample .Net App", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web app developed using React and ASP.NET", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.REACT ,Tag.CSHARP, Tag.ASPNET]},
-    {id: 3, name: "Web API Project", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Web API Project that was developed for a class project.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.CSHARP, Tag.ASPNET]},
-    {id: 4, name: "Chrome Extension", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Developed a chrome extension that tracks the prices of furniture.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVASCRIPT]},
-    {id: 5, name: "Mobile App", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Mobile app developed in java that tracks the departure and arrival of trains.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVA]}
+    { id: 0, name: "Management and Monitoring System of Tourist Activities", pictures: ["../../assets/Image1.png", "../../assets/Image2.png", "../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web application", description: "This system aims to standardize and centralize data on tourism activities from various institutional organizations.", tags: [Tag.JAVA] },
+    { id: 1, name: "Management and Monitoring System of Craft Activities", pictures: ["../../assets/Image1.png", "../../assets/Image2.png", "../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web application", description: "This system aims to standardize and centralize data on tourism activities from various institutional organizations.", tags: [Tag.JAVA, Tag.JavaEE] },
+    { id: 2, name: "Design and implementation of a Computerized Maintenance Management System", pictures: ["../../assets/Image1.png", "../../assets/Image2.png", "../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Desktop application", description: "As a final-year Master's student, I completed an internship at ELIT. As part of a two-person team, I contributed to the design and implementation of a Computerized Maintenance Management System (CMMS) in connected/disconnected mode with data synchronization.", tags: [Tag.SpringBoot] },
+    { id: 3, name: "Management final year projects for Master's students", pictures: ["../../assets/Image1.png", "../../assets/Image2.png", "../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web application", description: "As a final-year Bachelor's student and a member of a two-person team, I participated in the design and implementation of a web application for managing final year projects (P.F.E) for Master's students within the computer science department.", tags: [Tag.PHP] },
+    { id: 4, name: "Employee manager application", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Fullstack web application", description: "Fullstack web application", tags: [Tag.JAVASCRIPT]},
+    { id: 5, name: "Simple authentication system using basic auth and JWT", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Back-end project for user registration, login, logout, generating token", description: "Back-end project for user registration, login, logout, generating token", tags: [Tag.JAVA]},
+    { id: 6, name: "Portfolio", pictures: ["../../assets/Image1.png","../../assets/Image2.png","../../assets/Image3.png"], projectLink: "//www.github.com", summary: "Front-end project", description: "Front-end project", tags: [Tag.JAVA]}
   ];
 
   constructor() { }
 
-  getProjects(){
+  getProjects() {
     return this.projects;
   }
 
-  getProjectById(id : number) : Project | undefined{
+  getProjectById(id: number): Project | undefined {
     let project = this.projects.find(p => p.id === id);
     return project;
   }
 
-  getProjectByFilter(filterTags : Tag[]){
-    let filterProjects : Project[] = [];
+  getProjectByFilter(filterTags: Tag[]) {
+    let filterProjects: Project[] = [];
 
-    this.projects.forEach( function(project) {
+    this.projects.forEach(function (project) {
       let foundAll = true;
 
-      filterTags.forEach( function(filterTag) {
-        if(project.tags.includes(filterTag) == false){
+      filterTags.forEach(function (filterTag) {
+        if (project.tags.includes(filterTag) == false) {
           foundAll = false;
         }
       });
 
-      if(foundAll){
+      if (foundAll) {
         filterProjects.push(project);
       }
     });
@@ -49,36 +50,64 @@ export class ProjectsService {
 
   getAllTags(): Tag[] {
     return [
+      Tag.JAVA,
+      Tag.JavaFx,
+      Tag.JavaEE,
+      Tag.SpringBoot,
+      Tag.HTMLCSS,
+      Tag.JSF,
+      Tag.PrimeFaces,
+      Tag.Bootstrap,
       Tag.ANGULAR,
       Tag.TYPESCIPT,
-      Tag.JAVA,
       Tag.JAVASCRIPT,
-      Tag.PYTHON,
-      Tag.NODEJS,
-      Tag.CSHARP,
-      Tag.ASPNET,
-      Tag.REACT
+      Tag.PHP,
+      Tag.UML,
+      Tag.Git,
+      Tag.ScrumMethod
     ];
   }
 
-  getTagsByCatg(categori : string): Tag[] {
-    if(categori === 'languages'){
-      return [
-        Tag.TYPESCIPT,
-        Tag.JAVA,
-        Tag.JAVASCRIPT,
-        Tag.PYTHON,
-        Tag.CSHARP,
-      ];
-    }else{
-      return [
-        Tag.ANGULAR,
-        Tag.NODEJS,
-        Tag.ASPNET,
-        Tag.REACT
-      ];
+
+
+  getTagsByCatg(categori: string): Tag[] {
+    switch (categori.toLowerCase()) {
+      case 'languages':
+        return [
+          Tag.JAVA,
+          Tag.TYPESCIPT,
+          Tag.JAVASCRIPT,
+          Tag.PHP
+        ];
+
+      case 'frameworks':
+        return [
+          Tag.JavaFx,
+          Tag.JavaEE,
+          Tag.SpringBoot,
+          Tag.HTMLCSS,
+          Tag.JSF,
+          Tag.PrimeFaces,
+          Tag.Bootstrap,
+          Tag.ANGULAR
+        ];
+
+      case 'tools':
+        return [
+          Tag.Git
+        ];
+
+      case 'methodologies':
+        return [
+          Tag.UML,
+          Tag.ScrumMethod
+        ];
+
+      default:
+        return [];
     }
-    
+
+
   }
 
 
